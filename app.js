323aff20db275f64e5e7826f9215fab7a2e22d5e
shambu1329 <shambukrishna34@gmail.com>
2023-04-01 11:31:36 +0530

    completed

--- app.js
+++ app.js
@@ -33,5 +33,10 @@ app.post("/update",async(req,res)=>{
     res.send(data)
 })
 
+app.post("/delete",async(req,res)=>{
+    let data=await eModel.find(req.body)
+    res.send(data)
+})
+
 app.listen(3000)
 
